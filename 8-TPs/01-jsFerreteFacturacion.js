c97bc82b573ca6627cd44fc00fcc2c5c24ac32f5
ivishooo <iviote05@gmail.com>
2021-07-08 23:09:42 -0300

    Parciales 2019 hechos y tps 1 y 2 hechos

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -3,15 +3,32 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
-function Sumar () 
-{
-	
+
+function Sumar() {
+    let precio1 = parseFloat(txtIdPrecioUno.value);
+    let precio2 = parseFloat(txtIdPrecioDos.value);
+    let precio3 = parseFloat(txtIdPrecioTres.value);
+
+    resultado = parseFloat(precio1 + precio2 + precio3)
+    alert("El resultado de la suma es " + resultado)
 }
-function Promedio () 
-{
-	
+function Promedio() {
+    let precio1 = parseFloat(txtIdPrecioUno.value);
+    let precio2 = parseFloat(txtIdPrecioDos.value);
+    let precio3 = parseFloat(txtIdPrecioTres.value);
+
+    resultadoSuma = parseFloat(precio1 + precio2 + precio3)
+
+    promedio = parseFloat(resultadoSuma / 3)
+    alert("El promedio es " + promedio)
 }
-function PrecioFinal () 
-{
-	
+function PrecioFinal() {
+    let precio1 = parseFloat(txtIdPrecioUno.value);
+    let precio2 = parseFloat(txtIdPrecioDos.value);
+    let precio3 = parseFloat(txtIdPrecioTres.value);
+
+    resultadoSuma = parseFloat(precio1 + precio2 + precio3)
+    calculoIva = parseFloat(resultadoSuma * 21 / 100)
+    total = parseFloat(resultadoSuma + calculoIva)
+    alert("El resultado + IVA es " + total)
 }
--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -6,13 +6,28 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
-
+    let largoTerreno = parseFloat(txtIdLargo.value);
+    let anchoTerreno = parseFloat(txtIdAncho.value);
+    
+    let perimetroRectangulo = parseFloat((largoTerreno * 2) + (anchoTerreno * 2))
+    let rectanguloAlambre = parseFloat(perimetroRectangulo * 3);
+    alert("La cantidad de alambre a comprar es: " + rectanguloAlambre)
 }
 function Circulo () 
 {
-	
+	let radioTerreno = parseFloat(txtIdRadio.value);
+    let diametro = radioTerreno * 2;
+    let perimetro = Math.PI * diametro;
+    let totalAlambre = Math.ceil(perimetro * 3);
+    alert("Se necesitan " + totalAlambre + " para cubrir el terreno")
 }
 function Materiales () 
 {
-	
+    let largoTerreno = parseFloat(txtIdLargo.value);
+    let anchoTerreno = parseFloat(txtIdAncho.value);
+    
+    let area = parseFloat(largoTerreno * anchoTerreno);
+    let bolsasCemento = parseFloat(area * 2);
+    let bolsasCal = parseFloat(area * 3);
+    alert("La cantidad de bolsas a comprar es: " + bolsasCemento + " de cemento y " + bolsasCal + " de cal")
 }
